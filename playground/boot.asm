56e3634883d781040ad394ef179b7e552a14d32b
Ray Ank <ray.ank@blub.blub>
2021-03-19 22:34:05 +0100

    simple print of a string

--- playground/boot.asm
+++ playground/boot.asm
@@ -10,12 +10,15 @@
     mov ax, 0xb800 ; init video buffer
     mov es, ax
 
-    xor bx, bx
+    mov bx, 320 ; 2x160
     mov ax, 0x0f41
     call putc
     mov ax, 0x0f42
     add bx, 2
     call putc
+
+    mov si, msg
+    call printk
 
 hang:
     hlt
@@ -23,12 +26,48 @@ hang:
 
 
 ;---------------------------------------
+
+; si - message address
+printk:
+    push ax
+    push bx
+    push dx
+    xor ax, ax
+    xor bx, bx
+    mov bl, [xpos]
+    shl bx, 1 ; x2
+    mov al, [ypos]
+    mov dx, 160
+    mul dx
+    add bx, ax
+    mov ah, 0x0f ; black/white
+printk_load_next:
+    lodsb ; load to al, from [si]
+    cmp al, 0
+    je printk_end
+    mov ah, 0x0f ; black/white
+    mov [es:bx], ax
+    add bx, 2
+    jmp printk_load_next
+printk_end:
+    pop dx
+    pop bx
+    pop ax
+    ret
+
+
 ; ah - bgcolor
 ; al - fgcolor
 ; bx - index
 putc:
     mov [es:bx], ax
     ret
+
+;---------------------------------------
+xpos db 0       ; x-position in video buffer. is 2xbytes!
+ypos db 0       ; column in video buffer.
+
+msg db 'Hello Kernel. MyOS', 10, ' Version: 0.1', 10, 0
 
 
 times 510-($-$$) db 0	; fill rest with zeros
