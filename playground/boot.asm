e2615902d592c1c333b13980de57934ec7f41704
Ray Ank <ray.ank@blub.blub>
2021-03-22 20:47:55 +0100

    add keyhandler and interrupt (babystep osdev)

--- playground/boot.asm
+++ playground/boot.asm
@@ -15,6 +15,15 @@
 	cld	; clear direction
 	cli	; deaktivate interrupts	
 
+    ; set hardware interrupt keyhandler
+    mov bx, 0x09    ; interrupt 9
+    shl bx, 2       ; x4
+    xor ax, ax
+    mov gs, ax
+    mov [gs:bx], word keyhandler    ; set the new interuppt adress
+    mov [gs:bx+2], ds   ; set the segment
+
+
     call cls
 
     ;m_putc 0x0f41,320   ; 2x160, A
@@ -30,11 +39,38 @@
     mov cx, 0x1234
     call print_registers
 
+    sti     ; activate interrupts
+
 hang:
     hlt
     jmp hang
 
+
 ;--------- Functions --------------------
+
+keyhandler:
+   in al, 0x60          ; get key data
+   mov bl, al           ; save it
+   mov byte [port60], al
+ 
+   in al, 0x61          ; keybrd control
+   mov ah, al
+   or al, 0x80          ; disable bit 7
+   out 0x61, al         ; send it back
+   xchg ah, al          ; get original
+   out 0x61, al         ; send that back
+ 
+   mov al, 0x20         ; End of Interrupt
+   out 0x20, al         ;
+ 
+   and bl, 0x80         ; key released
+   jnz done             ; don't repeat
+ 
+   mov ax, [port60]
+   call printk_integer
+done:
+   iret
+
 %include "print.inc"
 
 ;---------------------------------------
@@ -47,6 +83,9 @@ msg_dx db 10,'DX - ',0
 msg_di db 10,'DI - ',0
 msg_si db 10,'SI - ',0
 msg_ip db 10,'IP - ',0
+
+port60   dw 0
+
 
 hex2char db '0123456789ABCDEF'
 
